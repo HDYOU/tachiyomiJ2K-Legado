713c5a6178a405099e3ba2ffd725ef3d0c074507
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-06-07 12:45:44 +0800

    Update write-signing-conf.sh

--- data/ci-sh/write-signing-conf.sh
+++ data/ci-sh/write-signing-conf.sh
@@ -30,6 +30,9 @@ fi
 
 sed -i '/ buildTypes {/a \        val signConfig = signingConfigs.getByName("keyStore")' app/build.gradle.kts
 
+sed -i 's/signingConfigs.getByName("debug")/signConfig/g' app/build.gradle.kts
+
+
 ##
 echo "cat app/build.gradle.kts:"
 echo ""
